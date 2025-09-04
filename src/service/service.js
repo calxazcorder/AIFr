7eb74656af167236aaac7da2f011d41ea39ef047
calxazcorder <your_email@example.com>
2025-09-04 15:32:24 +0400

    worrkr

--- src/service/service.js
+++ src/service/service.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.REACT_APP_ENV_VARIABLE || "http://localhost:5000";
-
+// sgs
 const submit = async (formData) => {
   const response = await fetch(`${API_BASE_URL}/predict`, {
     method: "POST",
